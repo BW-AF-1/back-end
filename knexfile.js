659775a1fffd22d015f616a89b7a3258af5b40e3
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-06-22 21:03:00 -0400

    finish a lot more routes and put more in readme

--- knexfile.js
+++ knexfile.js
@@ -65,10 +65,4 @@ module.exports = {
       }
     },
   },
-  pool: {
-    afterCreate: (conn, done) => {
-      // runs after a connection is made to the sqlite engine
-      conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-    }
-  }
 }
